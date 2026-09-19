7d31262d19d6b665d03ab55afc802ff99a5b676f
claau23 <claau1810@gmail.com>
2026-09-19 14:50:09 +0000

    Importar proyecto Wix

---
+++ .wix/types/uvkqt/uvkqt.d.ts
@@ -0,0 +1,30 @@
+/// <reference path="../masterPage/masterPage.d.ts" />
+type PageElementsMap = MasterPageElementsMap & {
+	"#section10": $w.HiddenCollapsedElement;
+	"#text15": $w.Text;
+	"#section9": $w.HiddenCollapsedElement;
+	"#box6": $w.Box;
+	"#text14": $w.Text;
+	"#text13": $w.Text;
+	"#section8": $w.HiddenCollapsedElement;
+	"#box5": $w.Box;
+	"#text12": $w.Text;
+	"#text11": $w.Text;
+	"#section7": $w.HiddenCollapsedElement;
+	"#box4": $w.Box;
+	"#text10": $w.Text;
+	"#text9": $w.Text;
+	"#section6": $w.HiddenCollapsedElement;
+	"#box3": $w.Box;
+	"#text8": $w.Text;
+	"#text7": $w.Text;
+	"#section5": $w.HiddenCollapsedElement;
+	"#box2": $w.Box;
+	"#text6": $w.Text;
+	"#text5": $w.Text;
+	"#section4": $w.HiddenCollapsedElement;
+	"#box1": $w.Box;
+	"#text4": $w.Text;
+	"#text3": $w.Text;
+	"#page1": $w.Page;
+}
---
+++ .wix/types/x92wi/x92wi.d.ts
@@ -0,0 +1,20 @@
+/// <reference path="../masterPage/masterPage.d.ts" />
+type PageElementsMap = MasterPageElementsMap & {
+	"#section6": $w.HiddenCollapsedElement;
+	"#box5": $w.Box;
+	"#text7": $w.Text;
+	"#text6": $w.Text;
+	"#text5": $w.Text;
+	"#section5": $w.HiddenCollapsedElement;
+	"#box2": $w.Box;
+	"#box4": $w.Box;
+	"#form1": $w.WixFormsV2;
+	"#text4": $w.Text;
+	"#box3": $w.Box;
+	"#googleMaps1": $w.GoogleMap;
+	"#section4": $w.HiddenCollapsedElement;
+	"#faqWidget1": $w.IFrame;
+	"#text3": $w.Text;
+	"#box1": $w.Box;
+	"#page1": $w.Page;
+}
